b928b06c13fb725642094745af335329666b66cc
Nicolas Peruch <nicolas_peruch@terceiros.sicredi.com.br>
2020-12-05 10:10:02 -0300

    Initial commit

---
+++ src/main/java/com/tproject/athletespoc/AthletesPocApplication.java
@@ -0,0 +1,13 @@
+package com.tproject.athletespoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthletesPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthletesPocApplication.class, args);
+	}
+
+}
